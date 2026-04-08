78d91a513e6c9a321f2d2caccff63335a0b57f45
waru <156133757+waruhachi@users.noreply.github.com>
2026-04-08 10:06:59 -0500

    Initial Commit

---
+++ BatteryBuddyReborn/BatteryBuddyRebornApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryBuddyRebornApp.swift
+//  BatteryBuddyReborn
+//
+//  Created by waru on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct BatteryBuddyRebornApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
